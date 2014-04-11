25266bfb65ab9dd5d48f8c9d43de45b0ee3f85fc
Tomasz Kowal <tomekowal@gmail.com>
2014-04-11 20:28:00 +0200

    Add problem constraint checking; change starting

--- hsa.py
+++ hsa.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python
 """hsa
 
 Usage:
-    hsa.py <problem_instance>
+    hsa.py <problem_instance> --hms=<hms> --hmcr=<hmcr> --parmax=<par> --parmin=<parmin> --ni=<ni>
+
+Options:
+    --hms=<hms>     Harmony memory size e.g. 10, 20, 30...
+    --hmcr=<hmcr>   Harmony memory consideration rate e.g. 0.6, 0.7, 0.8
+    --ni=<ni>       Number of improvisations e.g. 500, 1000, 2000
+    --parxmax=<parmax>  Maximal pitch adjustment rate e.g. 0.9
+    --parxmin=<parmin>  Minimal pitch adjustment rate e.g. 0.3
 
 """
 
@@ -34,8 +42,9 @@ def parse_problem_lines(lines):
     problem_instance['name'] = get_problem_name(lines)
     problem_instance['vehicle_number'] = vehicle_number
     problem_instance['capacity'] = capacity
-    #naming as in "Harmony Search Algorith for Vehicle Rougint Problem with Time Windows
+    #naming as in "Harmony Search Algorith for Vehicle Rougint Problem with Time Windows"
     #Esam Taha Yassen et al.
+    #http://scialert.net/qredirect.php?doi=jas.2013.633.638&linkid=pdf
     #t is travel time matrix t[0..customer_number][0..customer_number]
     #denoting travel time between t[i][j] (it is symmetric)
     #n is customer number (fixed to 100 in Solomon problem set)
@@ -68,6 +77,7 @@ def parse_problem_lines(lines):
     problem_instance['e'] = e
     problem_instance['l'] = l
     problem_instance['t'] = t
+    problem_instance['n'] = n
 
     return problem_instance
 
@@ -77,9 +87,133 @@ def parse_problem(path_to_file):
         lines = f.readlines()
         return parse_problem_lines(lines)
 
+def terminate():
+    return True
+
+#create random solution
+from random import shuffle
+def pfih(problem_instance):
+    n = problem_instance['n']
+    v = problem_instance['vehicle_number']
+    x = []
+    for i in range(n+1):
+        tmp1 = []
+        for j in range(n+1):
+            tmp2 = []
+            for k in range(v):
+                tmp2.append(0)
+            tmp1.append(tmp2)
+        x.append(tmp1)
+
+    y = []
+    for i in range(n+1):
+        tmp1 = []
+        for k in range(v):
+            tmp1.append(0)
+        y.append(tmp1)
+
+    customers_to_serve = range(n+1)
+    customers_to_serve.pop(0)
+    shuffle(customers_to_serve)
+
+    #take first customer and add to first route
+    k = 0
+    c = customers_to_serve.pop(0)
+    x[0][c][k] = 1
+    while customers_to_serve:
+        #put c in the first route
+        pass
+
+
+    print customers_to_serve
+    print n
+
+def check_constraints(x, problem_instance):
+    n = problem_instance['n']
+    #(2) if vehicle travelled from i to j, it also served j
+    for k in range(v):
+        for j in range(n+1):
+            total = 0
+            for i in range(n+1):
+                total = total + x[i][j][k]
+            if total != y[j][k]:
+                print "2"
+                return False
+
+    #(3) if vehicle travelled from i to j, it served i
+    for k in range(v):
+        for i in range(n+1):
+            total = 0
+            for j in range(n+1):
+                total = total + x[i][j][k]
+            if total != y[i][k]:
+                print "3"
+                return False
+
+    #(4) vehicle capacity is not exceeded
+    for k in range(v):
+        total = 0
+        for i in range(n+1):
+            total = total + y[i][k] * q[i]
+        if total > problem_instance['capacity']:
+            print "4"
+            return False
+    #(5) each customer is served only once
+    for i in range(n+1):
+        total = 0
+        for i in range(v):
+            total = total + y[i][k]
+        if total != 1:
+            print "5"
+            return False
+    #(6) every vehicle starts from depot
+    for k in range(v):
+        if y[0][k] != v:
+            print "6"
+            return False
+    #(7) next town is visited after first one is served
+    for i in range(n+1):
+        for j in range(n+1):
+            if i != j:
+                if a[i] + W[i] + s[i] + t[i][j] != a[j]:
+                    print "7"
+                    return False
+    #(8) sevice is started in time window
+    for i in range(n+1):
+        if t[i] > e[i] or l[i] > t[i]:
+            print "8"
+            return False
+    #(9) waiting time is either service start time - arriving time or 0
+    for i in range(n+1):
+        if W[i] != max(e[i] - a[i], 0):
+            print "9"
+            return False
+
+    return True
+
+#not finished
+def f(t, x, n, v):
+    """quality function, that we want to minimize - total travelled distance"""
+    total = 0
+    for i in range(n+1):
+        for j in range(n+1):
+            for k in range(v):
+                total = t[i][j] * x[i][j][k]
+
+
+
 from docopt import docopt
 if __name__ == "__main__":
     arguments = docopt(__doc__)
     problem_instance = parse_problem(arguments['<problem_instance>'])
-    print problem_instance
-
+    hms = int(arguments['--hms'])
+    hmcr = float(arguments['--hmcr'])
+    parmax = float(arguments['--parmax'])
+    parmin = float(arguments['--parmin'])
+    ni = int(arguments['--ni'])
+    generation = 0
+    print parmax, parmin, ni
+    #steps defined in "Harmony Search Algorith for Vehicle Rougint Problem with Time Windows"
+    #Esam Taha Yassen et al.
+    #http://scialert.net/qredirect.php?doi=jas.2013.633.638&linkid=pdf
+    print pfih(problem_instance)
